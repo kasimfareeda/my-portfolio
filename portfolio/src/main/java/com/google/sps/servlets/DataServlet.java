8a20ebd3b5d36136fdfe5559482ee2610f28e8fd
Fareeda <kasimfareeda@gmail.com>
2020-06-14 17:05:41 -0400

    Write javascript code to fetch data from server

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
