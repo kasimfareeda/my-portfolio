ad428ee7cde2c5c3bd3944ad16129c342fa6f387
Fareeda <kasimfareeda@gmail.com>
2020-08-05 01:56:34 -0400

    Completed blobstore implementation

--- portfolio/src/main/java/com/google/sps/servlets/BlogServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BlogServlet.java
@@ -5,10 +5,19 @@ import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
 import com.google.appengine.api.images.ServingUrlOptions;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
@@ -25,54 +34,10 @@ public class BlogServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // Get the Blobstore URL
         BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        String uploadUrl = blobstoreService.createUploadUrl("/blog");
+        String uploadUrl = blobstoreService.createUploadUrl("/image-data");
 
         response.setContentType("text/html");
         response.getWriter().println(uploadUrl);
-    }
-    
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Get the URL of the image that the user uploaded to Blobstore.
-        String imageUrl = getUploadedFileUrl(request, "upload");
-        System.out.println(imageUrl);
 
-    }
-
-    /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
-    private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
-        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
-        List<BlobKey> blobKeys = blobs.get("upload");
-
-        // User submitted form without selecting a file, so we can't get a URL. (dev server)
-        if (blobKeys == null || blobKeys.isEmpty()) {
-        return null;
-        }
-
-        // Our form only contains a single file input, so get the first index.
-        BlobKey blobKey = blobKeys.get(0);
-
-        // User submitted form without selecting a file, so we can't get a URL. (live server)
-        BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-        if (blobInfo.getSize() == 0) {
-        blobstoreService.delete(blobKey);
-        return null;
-        }
-
-        // We could check the validity of the file here, e.g. to make sure it's an image file
-        // https://stackoverflow.com/q/10779564/873165
-
-        // Use ImagesService to get a URL that points to the uploaded file.
-        ImagesService imagesService = ImagesServiceFactory.getImagesService();
-        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-        String url = imagesService.getServingUrl(options);
-
-        // GCS's localhost preview is not actually on localhost,
-        // so make the URL relative to the current domain.
-        if(url.startsWith("http://localhost:8080/")){
-        url = url.replace("http://localhost:8080/", "/");
-        }
-        return url;
     }
 }
